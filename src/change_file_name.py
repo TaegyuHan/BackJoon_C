13c8361a33c568d6dd4cc025a57e51c942df1b9f
hantaegyu <59700057+hantaegyu@users.noreply.github.com>
2021-07-20 21:56:17 +0900

    (폴더 정리) 파일이름변경 코드

---
+++ src/change_file_name.py
@@ -0,0 +1,13 @@
+
+# 폴더안의 파일 이름변경 코드
+
+import os
+
+path = "C:\github\BackJoon_C\Bronze IV"
+folder_list = os.listdir(path)
+
+for folder in folder_list:
+    os.chdir(path + '\\' + folder)
+    file_name = os.listdir(os.getcwd())[0]
+    print("변경완료 %s > %s", file_name, "README.md")
+    os.rename(file_name, "README.md")
