03fddb7c8dfa8d955a3e655c7944b319a8e5c165
hantaegyu <59700057+hantaegyu@users.noreply.github.com>
2021-09-16 19:45:24 +0900

    2805 나무자르기

--- CLASS/CLASS 2/2805 나무자르기/2805.py
+++ CLASS/CLASS 2/2805 나무자르기/2805.py
@@ -1,24 +1,25 @@
 import sys
 
-sys.stdin = open('./2805.txt')
-
+# sys.stdin = open('./2805.txt')
 
 tree_cnt, M = map(int, sys.stdin.readline().split())
 tree_size_list = list(map(int, sys.stdin.readline().split()))
 
-high = max(tree_size_list)
-low = 0
+end = max(tree_size_list)
+start = 0
+result = 0
 
-while(low < high-1):
-    mid = (low + high) // 2
+while (start <= end):
+    mid = (start + end) // 2
 
     # 2분탐색으로 얻은 값을 뺀것을
     # 더한 값
     tmp_sum = sum(map(lambda x: x - mid if (x - mid) > 0 else 0 , tree_size_list))
 
-    if tmp_sum > M:
-        low = mid
+    if tmp_sum >= M:
+        start = mid + 1
+        result = mid
     else:
-        high = mid
-    
-print(tmp_sum)
+        end = mid - 1
+
+print(result)
