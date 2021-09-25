88142ea572c260a2633a307aece891c027996590
hantaegyu <59700057+hantaegyu@users.noreply.github.com>
2021-09-25 16:28:07 +0900

    수정

--- 단계문제/정렬/1427 소트인사이드/1427.py
+++ 단계문제/정렬/1427 소트인사이드/1427.py
@@ -2,6 +2,6 @@ import sys
 
 # sys.stdin = open('./1427.txt')
 
-N = sorted(map(str, sys.stdin.readline()))
-for i in reversed(range(len(N))):
+N = sorted(map(str, sys.stdin.readline()), reverse=True)
+for i in range(len(N)):
   print(N[i], end="")
