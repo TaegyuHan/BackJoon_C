44740e48fd92787f44889448393be37461fcb68b
hantaegyu <59700057+hantaegyu@users.noreply.github.com>
2021-10-01 10:55:50 +0900

    18870 좌표 압축

---
+++ CLASS/CLASS 3/18870 좌표 압축/1_show_18870.py
@@ -0,0 +1,17 @@
+import sys
+
+# sys.stdin = open('./18870.txt')
+
+# 데이터 받기
+input_count = int(sys.stdin.readline())
+X_list = list(map(int, sys.stdin.readline().split()))
+
+# 데이터 key 정렬
+unique_value = sorted(set(X_list))
+
+# 테이블 만들기
+table = {key : index for index, key in enumerate(unique_value)}
+
+# 출력
+print(' '.join(map(lambda x: str(table[x]), X_list)))
+
