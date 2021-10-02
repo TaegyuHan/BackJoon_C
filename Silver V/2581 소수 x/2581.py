a2896a056a315bf04c3e3c42329c41cf8f4a7402
Taegyu Han <59700057+TaegyuHan@users.noreply.github.com>
2021-10-02 17:21:43 +0900

    2581 소수

--- Silver V/2581 소수 x/2581.py
+++ Silver V/2581 소수 x/2581.py
@@ -1,31 +1,27 @@
 import sys
 
-sys.stdin = open('./2581.txt')
+# sys.stdin = open('./2581.txt')
 
 M = int(sys.stdin.readline())
-N = int(sys.stdin.readline())
+N = int(sys.stdin.readline()) + 1
+number_set = set(i for i in range(M, N))
 
+sieve = [True] * N
+sieve[1] = False
+m = int(N ** 0.5)
 
-def prime_list(start, end):
-    # 에라토스테네스의 체 초기화: n개 요소에 True 설정(소수로 간주)
+for i in range(2, m + 1):
+    if sieve[i] == True:              # i가 소수인 경우
+        for j in range(i+i, N, i):  # i이후 i의 배수들을 False 판정
+            sieve[j] = False
 
-    sieve = [True] * end
+# 소수 목록 산출
+for i in range(M, N):
+    if sieve[i] != True:
+        number_set.remove(i)
 
-    # n의 최대 약수가 sqrt(end) 이하이므로 i=sqrt(end)까지 검사
-    m = int(end ** 0.5)
-    for i in range(2, m + 1):
-        if sieve[i] == True:              # i가 소수인 경우
-            for j in range(i+i, end, i):  # i이후 i의 배수들을 False 판정
-                sieve[j] = False
-
-    # 소수 목록 산출
-    return [i for i in range(start, end) if sieve[i] == True]
-
-
-list = prime_list(M, N)
-
-if len(list) > 0:
-    print(sum(list))
-    print(min(list))
+if len(number_set) != 0:
+    print(sum(number_set))
+    print(min(number_set))
 else:
     print(-1)
