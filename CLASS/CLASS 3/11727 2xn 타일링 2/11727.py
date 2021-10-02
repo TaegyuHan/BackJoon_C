d4f5a1e5efa2580982fb509dae325b4ed6928d5c
hantaegyu <59700057+hantaegyu@users.noreply.github.com>
2021-10-02 13:34:19 +0900

    11727 2xn 타일링

---
+++ CLASS/CLASS 3/11727 2xn 타일링 2/11727.py
@@ -0,0 +1,11 @@
+import sys
+
+# sys.stdin = open('./11727.txt')
+
+N = int(sys.stdin.readline())
+
+num_list = [1, 2]
+for i in range(2, N):
+    num_list.append(num_list[i - 2] + num_list[i - 1])
+
+print(num_list[N - 1]%10_007)
---
+++ CLASS/CLASS 3/11727 2xn 타일링 2/show_1_11727.py
@@ -0,0 +1,3 @@
+x,y=0,1
+exec('x,y=y,x+y;'*int(input()))
+print(y%10007)
