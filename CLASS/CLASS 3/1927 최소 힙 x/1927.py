0cc7671ca1e0b69356566e673894036e15835675
hantaegyu <59700057+hantaegyu@users.noreply.github.com>
2021-10-04 18:28:29 +0900

    해결중...

--- CLASS/CLASS 3/1927 최소 힙 x/1927.py
+++ CLASS/CLASS 3/1927 최소 힙 x/1927.py
@@ -2,55 +2,75 @@ import sys
 
 sys.stdin = open('./1927.txt')
 
-input_data_count = int(sys.stdin.readline())
+count = int(sys.stdin.readline())
 
 class Heap():
 
-  def __init__(self):
-      self.list = [0]
-      self.data_index = 0
-      self.len = 0
-
-  def show_list(self):
-      print(self.list)
-
-  def check_empty(self):
-      if self.data_index == 0 and self.len == 0:
-        return True
-      else:
-        return False
-
-  def compare_list(self):
-      index_a = self.data_index
-      index_b = self.data_index // 2
-      print(index_a,index_b)
-
-  def in_data(self, data):
-      self.list.append(data)
-      self.data_index += 1
-      self.len += 1
-
-      if self.len > 1:
-        self.compare_list()
+    def __init__(self) -> None:
+        self.data_list = [0]
+        self.index = 0
   
-  def out_data(self):
-      if self.check_empty() == True:
-          print(0)
-          return
+    def _swap(self, num1, num2) -> None:
+        tmp = self.data_list[num2]
+        self.data_list[num2] = self.data_list[num1]
+        self.data_list[num1] = tmp
 
-      print(self.list[self.data_index-1])
-      self.list[0] = self.list[self.data_index-1]
-      self.data_index -= 1
-      self.len -= 1
+    def _check_empty(self) -> bool:
+        if self.index == 0:
+            return True
+        else:
+            return False
 
+    def _up_sort(self) -> None:
+        a_index = self.index
+        while a_index > 1:
+            b_index = a_index//2
 
-if __name__ == '__main__':
-    heap = Heap()
-    heap.in_data(5)
-    heap.in_data(4)
-    heap.in_data(3)
-    heap.in_data(2)
-    heap.in_data(1)
-    heap.show_list()
-    heap.out_data()
+            if self.data_list[a_index] <= self.data_list[b_index]:
+                self._swap(a_index, b_index)
+                a_index = b_index
+            else: 
+              break
 
+    def _down_sort(self) -> None:
+        a_index = 1
+        while a_index <= self.index:
+            # print(a_index)
+            if (b_index := a_index*2) < self.index:
+                if self.data_list[a_index] >= self.data_list[b_index]:
+                    self._swap(a_index, b_index)
+                    a_index = b_index
+
+            if (b_index := a_index*2 + 1) < self.index:
+                if self.data_list[a_index] >= self.data_list[b_index]:
+                    self._swap(a_index, b_index)
+                    a_index = b_index
+
+            a_index = b_index
+
+            # print(self.data_list)
+
+    def enqueue(self, data: int) -> None:
+        self.data_list.append(data)
+        self.index += 1
+        self._up_sort()
+
+    def dequeue(self) -> None:
+        if self._check_empty():
+            print(0)
+            return
+        else:
+            tmp = self.data_list[1]
+            self.data_list[1] = self.data_list[self.index]
+            self.data_list[self.index] = tmp
+            print(self.data_list.pop())
+            self.index -= 1
+            p._down_sort()
+
+p = Heap()
+for _ in range(count):
+    print(p.data_list)
+    if (input := int(sys.stdin.readline())) == 0:
+        p.dequeue()
+    else:
+        p.enqueue(input)
