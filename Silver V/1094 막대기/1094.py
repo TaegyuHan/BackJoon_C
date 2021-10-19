4a591075e5fa06f438fa0278f5dec8ce9e38c6de
Taegyu Han <59700057+TaegyuHan@users.noreply.github.com>
2021-10-19 12:53:54 +0900

    1094 막대기 풀이중

---
+++ Silver V/1094 막대기/1094.py
@@ -0,0 +1,11 @@
+import sys
+
+sys.stdin = open("./1094.txt")
+stick = [int(sys.stdin.readline())]
+
+def div_stick(lst, index):
+    lst[(len(lst) - 1)] // 2
+    lst.append()
+    
+
+div_stick(stick)
