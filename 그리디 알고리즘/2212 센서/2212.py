f8e043a97bda4fb80f677cfe535a277099ae5bc6
Taegyu Han <59700057+TaegyuHan@users.noreply.github.com>
2021-11-13 20:53:13 +0900

    2212 센서

--- 그리디 알고리즘/2212 센서/2212.py
+++ 그리디 알고리즘/2212 센서/2212.py
@@ -1,4 +1,21 @@
 from sys import stdin as input
 
-input = open("./2212.txt")
+# input = open("./2212.txt")
 
+senser_cnt = int(input.readline())
+station_cnt = int(input.readline())
+sensers = sorted(map(int, input.readline().split()))
+
+if station_cnt > senser_cnt:
+    print(0)
+    exit()
+
+senser_distance = []
+for i in range(1, senser_cnt):
+    senser_distance.append(sensers[i] - sensers[i - 1])
+senser_distance.sort()
+
+for _ in range(station_cnt - 1):
+    senser_distance.pop()
+
+print(sum(senser_distance))
