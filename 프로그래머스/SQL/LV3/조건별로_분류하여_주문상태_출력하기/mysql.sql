832c1b6009bad1c1f1a86ef374479906e093ab6c
Taegyu <59700057+TaegyuHan@users.noreply.github.com>
2023-05-24 11:21:34 +0900

    2023-05-24 SQL 연습

---
+++ 프로그래머스/SQL/LV3/조건별로_분류하여_주문상태_출력하기/mysql.sql
@@ -0,0 +1,11 @@
+SELECT
+    F.ORDER_ID,
+    F.PRODUCT_ID,
+    DATE_FORMAT(F.OUT_DATE, "%Y-%m-%d") AS OUT_DATE,
+    CASE
+        WHEN DATE_FORMAT(F.OUT_DATE, "%m-%d") <= '05-01' THEN '출고완료'
+        WHEN F.OUT_DATE IS NULL THEN '출고미정'
+        ELSE '출고대기'
+    END AS '출고여부'
+FROM FOOD_ORDER AS F
+ORDER BY ORDER_ID;
---
+++ 프로그래머스/SQL/LV3/조건별로_분류하여_주문상태_출력하기/oracle.sql
@@ -0,0 +1,11 @@
+SELECT
+    F.ORDER_ID,
+    F.PRODUCT_ID,
+    TO_CHAR(F.OUT_DATE, 'YYYY-MM-DD') AS OUT_DATE,
+    CASE
+        WHEN TO_CHAR(F.OUT_DATE, 'MM-DD') <= '05-01' THEN '출고완료'
+        WHEN F.OUT_DATE IS NULL THEN '출고미정'
+        ELSE '출고대기'
+    END AS "출고여부"
+FROM FOOD_ORDER F
+ORDER BY ORDER_ID;
